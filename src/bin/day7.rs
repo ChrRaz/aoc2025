f8eb9698ebea0358da2759ff81a959613b813a07
Christian Rasmussen <chr@raznet.dk>
2025-12-07 20:56:26 +0100

    Day 7 Part 2: Use a Vec instead of a Map
    Probably good for performance idk.

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,5 +1,5 @@
-use std::collections::{BTreeMap, BTreeSet};
-use std::{io, mem};
+use std::collections::BTreeSet;
+use std::{io, iter, mem};
 
 fn main() {
     let input = io::read_to_string(io::stdin()).unwrap();
@@ -38,28 +38,33 @@ fn part1(input: &str) {
 
 fn part2(input: &str) {
     let mut lines = input.lines();
-    let mut tachyons: BTreeMap<_, _> = lines
-        .next()
-        .expect("there must be a starting line")
+    let first_line = lines.next().expect("there must be a starting line");
+    let mut tachyons: Vec<_> = first_line
         .chars()
         .enumerate()
-        .filter_map(|(pos, c)| (c == 'S').then_some((pos, 1u64)))
-        .collect();
-    let mut new_tachyons = BTreeMap::new();
+        .filter_map(|(pos, c)| (c == 'S').then_some(pos))
+        .fold(
+            iter::repeat_n(0, first_line.len()).collect(),
+            |mut acc, pos| {
+                acc[pos] = 1;
+                acc
+            },
+        );
+    let mut new_tachyons: Vec<_> = iter::repeat_n(0, first_line.len()).collect();
 
     let mut timelines = 1u64;
     for x in lines {
-        for (&i, &count) in &tachyons {
+        for (i, &count) in tachyons.iter().enumerate() {
             if (&x[i..=i]) == "^" {
                 timelines += count;
-                *new_tachyons.entry(i - 1).or_insert(count) += count;
-                *new_tachyons.entry(i + 1).or_insert(count) += count;
+                new_tachyons[i - 1] += count;
+                new_tachyons[i + 1] += count;
             } else {
-                *new_tachyons.entry(i).or_insert(count) += count;
+                new_tachyons[i] += count;
             }
         }
         mem::swap(&mut tachyons, &mut new_tachyons);
-        new_tachyons.clear();
+        new_tachyons.fill(0);
     }
     println!("Part 2: {timelines}");
 }
