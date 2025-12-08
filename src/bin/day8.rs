7c3ba167163df3c4603e2a3b3db00e839e343718
Christian Rasmussen <chr@raznet.dk>
2025-12-08 17:24:44 +0100

    Day 8 Part 2
    Very ugly solution for now.
    Just keep going after printing the answer for part 1.

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -31,21 +31,21 @@ fn main() {
     let points = parser.parse(&*input).unwrap();
 
     // Select the `capacity` shortest connections and sort them.
-    let edges_to_join = aoc25::n_smallest(
-        (0..points.len()).flat_map(|a| {
+    let mut edges_to_join = (0..points.len())
+        .flat_map(|a| {
             let points = &points; // rust is being weird about move closures
             (0..a).map(move |b| {
                 // Swap a and b here just so edges go from small to high index
                 Edge::new(points, b, a)
             })
-        }),
-        capacity,
-    )
-    .into_sorted_vec();
+        })
+        .collect::<Vec<_>>();
+    edges_to_join.sort();
 
     // Join up the circuits
     let mut circuit = (0..points.len()).collect::<Vec<_>>(); // Everyone is their own circuit.
-    for Edge(_, mut a, mut b) in edges_to_join {
+    let mut edges_to_join = edges_to_join.into_iter();
+    for Edge(_, mut a, mut b) in edges_to_join.by_ref().take(capacity) {
         // println!("Joining {x:3?} ({b} -> {a})");
         while a != circuit[a] {
             a = circuit[a];
@@ -71,7 +71,43 @@ fn main() {
     circuit_size.sort();
     let biggest = circuit_size.into_iter().rev().take(3).collect::<Vec<_>>();
     dbg_inline!("{:?}": &biggest);
-    println!("Part 1: {}", biggest.iter().product::<u64>())
+    println!("Part 1: {}", biggest.iter().product::<u64>());
+
+    for Edge(_, mut a, mut b) in edges_to_join {
+        let old_a = a;
+        let old_b = b;
+
+        while a != circuit[a] {
+            a = circuit[a];
+        }
+        while b != circuit[b] {
+            b = circuit[b];
+        }
+        circuit[b] = circuit[a];
+
+        // Flatten the structure for easy counting
+        for i in 0..circuit.len() {
+            while circuit[i] != circuit[circuit[i]] {
+                circuit[i] = circuit[circuit[i]];
+            }
+        }
+        // Count the circuit sizes.
+        let mut circuit_size = iter::repeat_n(0u64, points.len()).collect::<Vec<_>>();
+        for &x in &circuit {
+            circuit_size[x] += 1;
+        }
+        circuit_size.sort();
+        if circuit_size
+            .iter()
+            .enumerate()
+            .filter_map(|(i, &x)| (x > 0).then_some(i))
+            .count()
+            == 1
+        {
+            println!("Part 2: {}", u64::from(points[old_a].0) * u64::from(points[old_b].0));
+            break;
+        }
+    }
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
